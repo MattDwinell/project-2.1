7056e992877d612bf5bfa6c839b2d6fec6961f4f
janiello <janiello417@gmail.com>
2019-07-02 19:34:40 -0400

    added an ascending order specification to the json query in apiRoutes so the scores show up in order from best to worst

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,11 @@
 var User = require("../models/users.js");
+var sequelize = require("sequelize");
 
 module.exports = function(app) {
     app.get("/api/scores", function(request, response) {
-        User.findAll({}).then(function(results) {
+        User.findAll({
+            order: sequelize.literal("userTimes ASC")
+        }).then(function(results) {
             response.json(results);
         });
     });
